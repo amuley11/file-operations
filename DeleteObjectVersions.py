4f9a32ee4964dc919103d1214b76df42a9f997df
Abhishek Muley <73030681+amuley11@users.noreply.github.com>
2021-09-03 15:48:35 +0800

    Update DeleteObjectVersions.py

--- DeleteObjectVersions.py
+++ DeleteObjectVersions.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
     threshold_dt = (datetime.today() - timedelta(days=RetentionPeriod)).strftime('%Y-%m-%d') # Getting the date for previous date
 
     obj = s3.list_object_versions(Bucket = S3Bucket)
-    for i in range(0,len(obj.keys())):
+    for i in range(0,len(obj['Versions'])):
         if threshold_dt >= obj['Versions'][i]['LastModified'].strftime('%Y-%m-%d') :
             obj_name = obj['Versions'][i]['Key']
             obj_ver = obj['Versions'][i]['VersionId']
@@ -22,5 +22,8 @@ def lambda_handler(event, context):
             del_counter += 1
 
     del_file_str = str(del_file_list).strip('[]')
-    print(str(del_counter) + " files which were created on or before the threshold date of - " + threshold_dt + " have been deleted. Here are their names & version ids - " + del_file_str)
+    if del_counter > 0:
+        print(str(del_counter) + " files which were created on or before the threshold date of - " + threshold_dt + " have been deleted. Here are their names & version ids - " + del_file_str)
+    else:
+        print("No files were created on or before " + threshold_dt + ", hence none deleted.")
     return "Process completed successfully."
